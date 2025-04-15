e04432af58e6c978e0b4dd67b01704c6cb4f4b20
José Gilberto Trujillo Melián <jose.hoc@gmail.com>
2025-04-15 22:34:39 +0100

    hardening

--- src/core/pascalcase.ts
+++ src/core/pascalcase.ts
@@ -4,11 +4,11 @@ export function topascalcase(inputString: string): string {
   }
 
   let resultArray = inputString.split(/[-_ ]+/);
-  resultArray = resultArray.map((word) => Capitalize(word, inputString));
+  resultArray = resultArray.map((word) => Capitalize(word));
 
   return resultArray.join('');
 }
 
-function Capitalize(word: string, inputString: string) {
-  return word[0].toUpperCase() + word.slice(1, inputString.length);
+function Capitalize(word: string) {
+  return word[0].toUpperCase() + word.slice(1, word.length);
 }
